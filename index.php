821a85c8b19ee8f2439d5ae69888579e40899cea
sconnole <seanconnole32@gmail.com>
2020-05-16 13:22:55 -0600

    Adding some starter files

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Sean Connole</title>
+  <meta name="description" content="">
+  <meta name="author" content="Sean Connole">
+
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+  <script src="libs/js/utils.js"></script>
+</body>
+
+
+</html>
